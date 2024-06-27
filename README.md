c7cb79c1dc3cb699e945b6d124492733fbf0a951
User Name <annakirath@gmail.com>
2024-06-27 23:35:01 +0100

    Finished up the styling of my game. Cleaned up my HTML, JS and CSS from comments and errors. Updated my readme file

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # mastermind-game
-Screenshot/Logo: A screenshot of your app or a logo.
+A screenshot of your app or a logo.
 
-Your game’s name: Include a description of your game and what it does. Background info about the game and why you chose it is a nice touch.
-- Mastermind is a code-breaking game. The goal is for the player to guess a generated code of colored pegs. The player has eight attempts to do so. After each guess, feedback is given. A black peg will indicate the player guessed correct color in correct position. While a feedback peg of white will indicate the player guessed the correct color but in the wrong position.
+Description of the game and why I chose it for my project:
+- Mastermind is a code-breaking game. The goal is for the player to guess a generated code of colored pegs. The player has eight attempts to do so. After each guess, feedback is provided:
+A black peg will indicate the player guessed correct color in the correct position, while a pink
+peg will indicate the player guessed the correct color but in the wrong position.
 
-- I chose Mastermind as my game project because I like the logic of the game. I was also interested in building a single player game, since we've covered multiplayer game in class like tic-tac-toe.   
+- I chose Mastermind as my game project because I like the logical challenge of the game. Addiotionally, I was also interested in developing a single-player game, as our class primarily focused on multiplayer games like tic-tac-toe.   
 
 Getting started: Include a link to your deployed game and any instructions you deem important. This should also contain a link to your planning materials.
 
-Attributions: This section should include links to any external resources (such as libraries or assets) you used to develop your application that require attribution. You can exclude this section if it does not apply to your application.
-
-Technologies used: List of the technologies used, for example: JavaScript, HTML, CSS, etc.
+Technologies used:
 For the completion of this project, JavaScript, HTML, CSS and Audio has been used.
 
-Next steps: Planned future enhancements (stretch goals).
+Future enhancements: 
+Future enhancements for this Mastermind project include:
+- Styling omprovements: Enhancing the overall design of the game to add a personal touch with custom colors, as the current color scheme is generic.
+- Player feedback: Improving the visibility of player attempts by adding level indicators (1-8) in the middle of the grid.
--- js/app.js
+++ js/app.js
@@ -1,35 +1,25 @@
-/*-------------------------------- Constants --------------------------------*/
 const rows = 8;
-const columns = 9; /* Maybe add two columns consts for feedbackPegs and may need a second one for static guess row */
+const columns = 9; 
 const totalSquareCount = columns * rows;
-const colorOptions = ['green', 'red', 'blue', 'brown', 'orange', 'yellow']; /* These are my chosen colors that are hidden and has to be guessed */
+const colorOptions = ['green', 'red', 'blue', 'brown', 'orange', 'yellow']; 
 const maximumAttempts = 7;
 
-
-/*-------------------------------- Variables (state) --------------------------------*/
-let hiddenColors; /* the array of generated colors */
-let playerAttempts; /* this starts at 0 since player hasn't used any attempts when starting game */
-let playerGuess; /* the array of the players guesses */
+let hiddenColors; 
+let playerAttempts; 
+let playerGuess; 
 let targetCell;
 let currentRow;
 let feedbackSquare;
 let winAudio = new Audio('./audio/Winner.wav');
 let loseAudio = new Audio('./audio/Game over.wav');
 
-
-/*------------------------ Cached Element References ------------------------*/
 const grid = document.querySelector('.grid');
-/* console.log(grid) */
-const squareElements = []; //This will contain all of the divs representing squares on my display
+const squareElements = [];
 const displayResult = document.querySelector('#display-result');
-/* console.log(displayResult) */
 const undoBtn = document.querySelector('#undo-button');
 const resetBtn = document.querySelector('#reset-button');
-/* console.log(undoBtn)
-console.log(resetBtn) */
 const colorPegsBtn = document.querySelectorAll('.color-peg');
 
-/*------------------------ Grid Creation ------------------------*/
 for (let i = rows -1; i >= 0; i--) {
     const row = document.createElement('div')
     row.classList.add('row')
@@ -46,9 +36,8 @@ for (let i = rows -1; i >= 0; i--) {
     grid.appendChild(row)
 }
 
-/*-------------------------------- Functions --------------------------------*/
 function init() {
-    hiddenColors = colorReveal();
+    hiddenColors = colorGenerator();
     playerAttempts = 0;
     playerGuess = [];
     targetCell = 0;
@@ -60,24 +49,17 @@ function init() {
     hiddenColorElements.forEach(element => {
         element.style.backgroundColor = 'white';
     });
-    console.log(hiddenColors);
 };
 
 init();
 
-/* 
-function render() { ------- remember to look into render if it is strictly necessary for this game/code
-    updateDisplay();
-    updateResult();
-} */
-
-function colorReveal() {
-    let hidden = []; /* this will initialize my empty array that will keep my generated colors once they're generated  */
-    for (let i = 0; i < 4; i++) { /* Choosing a for loop that runs 4 times (the amount of hidden pegs) */
-        let randomColor = colorOptions[Math.floor(Math.random() * colorOptions.length)]; /* using Math.floor and math.random methods to select random colors from my colors array */
-        hidden.push(randomColor); /* this pushes the random selected color to the empty hidden array */
+function colorGenerator() {
+    let hidden = [];
+    for (let i = 0; i < 4; i++) {
+        let randomColor = colorOptions[Math.floor(Math.random() * colorOptions.length)];
+        hidden.push(randomColor); 
     }
-    return hidden; /* this is returning the hidden array now with the generated colors */
+    return hidden;
 }
 
 function updateDisplay() {
@@ -110,6 +92,10 @@ function updateDisplay() {
     }
 }
 
+function checkPlayerGuess() {
+    return playerGuess.every((color, index) => color === hiddenColors[index]);
+}
+
 function revealHiddenColors() {
     for (let i = 0; i < hiddenColors.length; i++) {
         const hiddenColorElements = document.getElementById(`hidden-color-${i+1}`);
@@ -119,18 +105,6 @@ function revealHiddenColors() {
     }
 }
 
-function checkPlayerGuess() {
-    return playerGuess.every((color, index) => color === hiddenColors[index]); /* using array every method to check if playerGuess chose the same color pegs as the hiddenColors have generated (both the color and at the same index in the array) */
-}
-/* 
-function updateResult() {
-    if (checkPlayerGuess()) {
-        displayResult.innerText = 'Congratulations, you won!';
-    } else if (playerAttempts >= maximumAttempts) {
-        displayResult.innerText = `Game over! The correct color pegs were ${hiddenColors.join(', ')}.`;
-    }
-} */
-
 function clearDisplay() {
     for (let i = 0; i < squareElements.length; i++) {
         squareElements[i].style.backgroundColor = 'white';
@@ -138,7 +112,6 @@ function clearDisplay() {
     displayResult.innerText = '';
 }
 
- /* For EACH player guess, check color and index = black & just color = white. How to update display only after guess? */
 function feedbackPegs() {
     let usedSquare = new Set();
     for (let i = 0; i < playerGuess.length; i++) {
@@ -147,7 +120,7 @@ function feedbackPegs() {
             if (feedbackCell) {
                 feedbackCell.style.backgroundColor = 'black';
                 feedbackSquare++;
-                usedSquare.add(i)
+                usedSquare.add(i);
             }
         }
     }
@@ -155,7 +128,7 @@ function feedbackPegs() {
         if (hiddenColors.includes(playerGuess[i])) {
             const feedbackCell = document.querySelector(`.row[data-row="${currentRow}"] .sqr[data-column="${feedbackSquare}"]`);
             if (feedbackCell) {
-                feedbackCell.style.backgroundColor = 'pink';
+                feedbackCell.style.backgroundColor = 'hotpink';
                 feedbackSquare++;
                 break;
             }
@@ -163,14 +136,10 @@ function feedbackPegs() {
     }
 }
 
-/* maybe reuse .every in checkPlayerGuess but remove index? 
-        however not sure how to not count colors twice for guesses with 2x of the same color
-        Also can't count the black pegs*/
-/*----------------------------- Event Listeners -----------------------------*/
 resetBtn.addEventListener('click', init);
 
 undoBtn.addEventListener('click', () => {
-    if (checkPlayerGuess() || playerAttempts >= maximumAttempts) {
+    if (checkPlayerGuess() || playerAttempts > maximumAttempts) {
     } else if (playerGuess.length > 0) {
         playerGuess.pop();
         const previousColumn = playerGuess.length;
@@ -184,21 +153,10 @@ undoBtn.addEventListener('click', () => {
 
 colorPegsBtn.forEach(button => {
     button.addEventListener('click', () => {
-        const color = button.dataset.color; /* this part is defining the actual color from my data-color in html and CSS, without it my event listener wont work */
+        const color = button.dataset.color;
         if (playerGuess.length < 4) {
             playerGuess.push(color);
-            console.log(playerGuess);
             updateDisplay();
         }
     });
 });
-
-
-/*----------------------------- Calling the functions -----------------------------*/
-/* render();
-updateResult();
-colorReveal();
-checkPlayerGuess();
-updateDisplay(); */
-/* feedbackPegs(); */
-/* revealHiddenColors(); */
